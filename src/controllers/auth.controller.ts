e493a68d34b4faf7a508e45f249c839bad6eff91
Ariel Llanita <llanitaariel@gmail.com>
2022-06-29 16:30:34 +0800

    use cookie parser and exclude password in response

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,6 +5,10 @@ import AuthService from '@services/auth.service';
 import { LoginDto } from '@/dtos/auth.dto';
 import { loginLimit } from '@/middlewares/loginLimit.middleware';
 import codesModel from '@/models/codes.model';
+import { HttpException } from '@/exceptions/HttpException';
+import userModel from '@/models/users.model';
+import { isEmpty } from '@/utils/util';
+import { add } from 'date-fns';
 
 class AuthController {
   public readonly authService = new AuthService();
@@ -13,12 +17,12 @@ class AuthController {
     try {
       const userData: LoginDto = req.body;
       const remainingLoginAttempts = req['rateLimit']?.remaining;
-      const { findUser, token, cookie } = await this.authService.login(userData, remainingLoginAttempts);
+      const { user, token } = await this.authService.login(userData, remainingLoginAttempts);
 
       loginLimit.resetKey(req.ip);
 
-      res.setHeader('Set-Cookie', [cookie]);
-      res.status(200).json({ data: findUser, token, message: 'Login successfully!' });
+      res.cookie('Authorization', token, { expires: add(new Date(), { days: 3 }), httpOnly: true, sameSite: 'none', secure: true });
+      res.status(200).json({ data: user, token, message: 'Login successfully!' });
     } catch (error) {
       next(error);
     }
@@ -26,8 +30,8 @@ class AuthController {
 
   public logOut: RequestHandler = async (req, res, next) => {
     try {
-      res.setHeader('Set-Cookie', [`Authorization=; Max-age=0; HttpOnly; SameSite=None; Secure; Path=/;`]);
-
+      res.cookie('Authorization', '', { maxAge: 0, sameSite: 'none', secure: true, httpOnly: true })
+      
       res.status(200).json({ message: 'Logout successfully' });
     } catch (error) {
       next(error);
@@ -38,6 +42,42 @@ class AuthController {
     try {
       const userData: User = req.user;
       res.status(200).json({ data: userData, message: 'Verify jwt' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public resetPassword: RequestHandler = async (req, res, next) => {
+    try {
+      const { password, userID } = req.body;
+      await userModel.findByIdAndUpdate(userID, { $set: { password } });
+
+      res.sendStatus(200);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public verifyResetPwId: RequestHandler = async (req, res, next) => {
+    try {
+      const { code } = req.params;
+      if (isEmpty(code)) throw new HttpException(400, 'Invalid Link');
+
+      const validID = await codesModel.findOne({ code }).lean();
+      if (!validID) throw new HttpException(409, 'Invalid Link');
+      await codesModel.findByIdAndRemove(validID._id);
+
+      res.status(200).json({ userID: validID.user, message: 'Reset password ID is valid' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public createResetPwId: RequestHandler = async (req, res, next) => {
+    try {
+      await this.authService.create_reset_pw_id(req.body?.email);
+
+      res.status(201).json({ message: 'Reset password ID created successfully!' });
     } catch (error) {
       next(error);
     }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
-import {  SECRET_KEY } from '@config';
+import { CORS_CONFIG, SECRET_KEY } from '@config';
 import { HttpException } from '@exceptions/HttpException';
 import { DataStoredInToken } from '@interfaces/auth.interface';
 import { User } from '@interfaces/users.interface';
@@ -8,47 +8,65 @@ import userModel from '@models/users.model';
 import { isEmpty } from '@utils/util';
 import { LeanDocument } from 'mongoose';
 import codesModel from '@/models/codes.model';
+import { v4 as uuidv4 } from 'uuid';
+import { sendEmail } from '@/utils/mailer';
 
 class AuthService {
-  public async login(userData: any, remainingAttempts: any): Promise<{ cookie: string; findUser: User; token: string }> {
+  public async login(userData: any, remainingAttempts: any): Promise<{ user: User; token: string }> {
     if (isEmpty(userData)) throw new HttpException(400, 'Please provide the correct data.');
 
     const validEmail: LeanDocument<User> = await userModel.findOne({ email: userData.email }).lean();
     if (!validEmail) throw new HttpException(409, 'Invalid email');
-
-    // determine if account is locked
-    const isLocked = await codesModel.exists({ user: validEmail._id });
-    // if locked ask for the code and verify
-    if (isLocked) {
-      if (isEmpty(userData?.code)) throw new HttpException(429, 'Please provide a code');
-      
-      const validCode = await codesModel.exists({ user: validEmail._id, code: userData?.code });
-      if (!validCode) throw new HttpException(409, 'Invalid code.');
-
-      // remove code after verification
-      await codesModel.findByIdAndRemove(isLocked);
-    }
 
     const validPassword: boolean = await compare(userData.password, validEmail.password);
     if (!validPassword) {
       throw new HttpException(409, `Invalid password, ${+remainingAttempts + 1} remaining attempt(s)`);
     }
 
-    const tokenData = this.createToken(validEmail);
-    const cookie = this.createCookie(tokenData);
+    const isLockedAccount = await codesModel.exists({ user: validEmail._id, type: 'ACCOUNT LOCK' });
+    if (isLockedAccount) {
+      if (isEmpty(userData?.code)) throw new HttpException(429, 'Please provide a code');
 
-    return { cookie, findUser: validEmail, token: tokenData };
+      const validCode = await codesModel.exists({ user: validEmail._id, code: userData?.code });
+      if (!validCode) throw new HttpException(409, 'Invalid code.');
+
+      // remove code after verification
+      await codesModel.findByIdAndRemove(isLockedAccount);
+    }
+
+    const tokenData = this.createToken(validEmail);
+    delete validEmail.password;
+
+    return { user: validEmail, token: tokenData };
+  }
+
+  public async create_reset_pw_id(email: string) {
+    if (isEmpty(email)) throw new HttpException(400, 'Please provide an email');
+
+    const user = await userModel.findOne({ email }).lean();
+    if (!user) throw new HttpException(409, 'Invalid email');
+
+    const code = uuidv4();
+    const passwordID = await codesModel.create({ user: user._id, type: 'RESET PASSWORD', code });
+
+    await sendEmail({
+      recipientEmail: user.email,
+      subject: 'Reset Password 🔓',
+      text: `Good day! To reset your account's password please click the link to proceed. Please note that this link is only available for 15 mins and afterwards will be automatically invalidated.\n\n${CORS_CONFIG.origin[0]}/reset-password/${code}`
+    });
+
+    return passwordID;
   }
 
   protected createToken(user: User): string {
     const dataStoredInToken: DataStoredInToken = { _id: user._id };
     const secretKey: string = SECRET_KEY;
-    
-    return sign(dataStoredInToken, secretKey);
+
+    return sign(dataStoredInToken, secretKey, { expiresIn: '3 days' });
   }
 
   protected createCookie(tokenData: string): string {
-    return `Authorization=${tokenData}; HttpOnly; SameSite=None; Secure; Path=/;`;
+    return `Authorization=${tokenData}; Max-age=; HttpOnly; SameSite=None; Secure; Path=/;`;
   }
 }
 
